a8119e00f2f1b29fcdb59575d62f3bdd620762e4
André Rosas <andre.lt.rosas@gmail.com>
2025-09-13 21:01:33 -0300

    UPDATE: "Update Vitest and deploy setup"

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mini-seller-react-MVP/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   css: {
     postcss: './postcss.config.js'
   }
